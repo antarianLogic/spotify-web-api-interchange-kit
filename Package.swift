67c3af54994071817d14edfe1b6ba75fca4c88e8
Carl Sheppard <carl@antarianlogic.com>
2023-09-13 14:32:15 -0700

    temporarily turned on StrictConcurrency warnings but left it commented out for future checking

--- Package.swift
+++ Package.swift
@@ -9,23 +9,20 @@ let package = Package(
         .macOS(.v14)
     ],
     products: [
-        .library(
-            name: "SpotifyAPIKit",
-            targets: ["SpotifyAPIKit"]),
+        .library(name: "SpotifyAPIKit",
+                 targets: ["SpotifyAPIKit"]),
     ],
     dependencies: [
         .package(url: "https://github.com/antarianLogic/rest-web-service", from: "0.6.0"),
         .package(url: "https://github.com/antarianLogic/json-utils", from: "0.3.0")
     ],
     targets: [
-        .target(
-            name: "SpotifyAPIKit",
-            dependencies: [.product(name: "RESTWebService", package: "rest-web-service"),
-                           .product(name: "JSONUtils", package: "json-utils")],
-            resources: [.copy("DataPresets/JSON")]
-        ),
-        .testTarget(
-            name: "SpotifyAPIKitTests",
-            dependencies: ["SpotifyAPIKit"]),
+        .target(name: "SpotifyAPIKit",
+                dependencies: [.product(name: "RESTWebService", package: "rest-web-service"),
+                               .product(name: "JSONUtils", package: "json-utils")],
+                resources: [.copy("DataPresets/JSON")]),
+//                swiftSettings: [.enableExperimentalFeature("StrictConcurrency")]),
+        .testTarget(name: "SpotifyAPIKitTests",
+                    dependencies: ["SpotifyAPIKit"]),
     ]
 )
